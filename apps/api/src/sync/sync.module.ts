0595d0832b9d4509f18e830850c93a4cfad84599
Marcos Moura <marcosvmmoura@gmail.com>
2025-08-05 05:18:26 +0200

    fix: imports

--- apps/api/src/sync/sync.module.ts
+++ apps/api/src/sync/sync.module.ts
@@ -1,9 +1,10 @@
 import { Module, forwardRef } from '@nestjs/common';
 
-import { AssetModule } from '../asset';
-import { MarketDataModule } from '../market-data';
-import { UserSettingsModule } from '../user-settings';
-import { YnabModule } from '../ynab';
+import { AssetModule } from '@/asset';
+import { MarketDataModule } from '@/market-data';
+import { UserSettingsModule } from '@/user-settings';
+import { YnabModule } from '@/ynab';
+
 import { SyncService } from './sync.service';
 
 @Module({
